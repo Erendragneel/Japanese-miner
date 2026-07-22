050c98594ca7ad9ee88b064b66e2b3c7a5db788a
Erendragneel <elijio.villa@gmail.com>
2026-07-22 11:37:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='japanese-miner-v5.1';
-const ASSETS=['./','./index.html','./styles.css?v=5.1','./v5.css?v=5.1','./game.js?v=5.1','./v5.js?v=5.1','./manifest.webmanifest','./assets/favicon.svg'];
+const CACHE='japanese-miner-v5.2';
+const ASSETS=['./','./index.html','./styles.css?v=5.2','./v5.css?v=5.2','./game.js?v=5.2','./v5.js?v=5.2','./manifest.webmanifest','./assets/favicon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));});
