d542508ada783c1a1554a1b596da5e67ab4b456c
Erendragneel <elijio.villa@gmail.com>
2026-08-04 17:44:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='japanese-miner-v6.4.55-accessory-portrait-refit';
+const CACHE_NAME='japanese-miner-v6.4.57-full-portrait-accessories';
 const APP_SHELL=[
   './',
   './index.html',
